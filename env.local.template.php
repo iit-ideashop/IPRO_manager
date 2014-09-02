da66fe3c52a7422f7b5691ef1faa2d9e6ec2cf83
Bartlomiej Dworak <BDworak@hawk.iit.edu>
2014-09-02 14:53:02 -0500

    added the environemtns file template file

---
+++ env.local.template.php
@@ -0,0 +1,20 @@
+<?php
+##rename this file to .env.local.php 
+return array(
+
+    'DATABASE_HOST' => '',
+    'DATABASE_USER' => '',
+    'DATABASE_PASSWORD' => '',
+    'DEBUG' => 'FALSE',
+    ## Default App URL is http://localhost for dev
+    'APP_URL' => 'http://localhost',
+    ## Mail driver setup, mandrill is mandrill 
+    'MAIL_DRIVER' => 'mandrill',
+    'MAIL_SMTP' => 'smtp.mandrillapp.com',
+    'MAIL_PORT' => '587',
+    'MAIL_FROM' => array('address' => 'ipro@iit.edu', 'name' => 'IPRO Manager'),
+    'MAIL_SMTP_USER' => 'ipro@iit.edu',
+    'MAIL_SMTP_PASS' => '',
+    'MAIL_MANDRILL_APIKEY' => ''
+);
+
