274d4c957d6574697e9449a5020b632b53866fc3
= <=>
2014-11-03 12:18:27 -0600

    fixing header file format

--- app/views/admin/reports/generator.blade.php
+++ app/views/admin/reports/generator.blade.php
@@ -7,9 +7,9 @@
  */
     //Save Excel 2007 file
     $objWriter = new PHPExcel_Writer_Excel2007($objPHPExcel);
-    header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    //header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
     //header('Content-Type: application/octet-stream');
-    //header('Content-type: application/xlsx');
+    header('Content-type: application/vnd.ms-excel');
     header('Content-Disposition: attachment; filename='.$excel_filename);
     $objWriter->save("php://output");
 ?>
