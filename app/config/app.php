05c8539a528603be12e234c9669afc8ea3c9799b
Bartlomiej Dworak <BDworak@hawk.iit.edu>
2015-02-03 21:00:54 -0600

    Removed composer laravel-ide-helper from app.php config

--- app/config/app.php
+++ app/config/app.php
@@ -122,7 +122,6 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
                 'Artdarek\OAuth\OAuthServiceProvider',
-		'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
 
 	),
 
