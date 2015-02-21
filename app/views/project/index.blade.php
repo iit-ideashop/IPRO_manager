5d406c364ea1a2c7af59273c209243e8b5ce85b7
Bartlomiej Dworak <BDworak@hawk.iit.edu>
2015-02-20 19:08:07 -0600

    Some dev work for the ipro nav

--- app/views/project/index.blade.php
+++ app/views/project/index.blade.php
@@ -2,7 +2,46 @@
 @include('layouts.dataTables')
 
 @section('content')
-            <h1 class="page-header">{{ $class->UID }}<div class="pull-right">Account: ${{ number_format($account->Balance,2) }} </div></h1>
+
+            <div class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#project-nav-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand">{{ $class->UID }}</a>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="project-nav-collapse">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#">Dashboard</a></li>
+                            <li><a href="#">Orders</a></li>
+                            <li><a href="#">Roster</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Actions <span class="caret"></span></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">New Order</a></li>
+                                    <li><a href="#">New Budget Request</a></li>
+                                    <li><a href="#"></a></li>
+                                    <li class="dropdown-header">Instructor Actions</li>
+                                    <li><a href="#">Group Management</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+
+                        <div class="pull-right navbar-brand">Account: ${{ number_format($account->Balance,2) }}</div>
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </div>
+
+
+
+
+
 
           @include('model_tables.orders')
           
