66f3db2511a4f0c42b30d460982f263cba586af9
BDworak <bdworak@hawk.iit.edu>
2015-04-08 00:56:17 -0500

    bug fix for database name on linux systems

--- app/models/PrintSubmission.php
+++ app/models/PrintSubmission.php
@@ -1,7 +1,7 @@
 <?php
 use LaravelBook\Ardent\Ardent;
 class PrintSubmission extends Ardent {
-    protected $table = 'printsubmissions';
+    protected $table = 'printSubmissions';
     public function getStatus()
     {
         return DB::table('printsubmissionstatuses')->where('id', $this->Status)->pluck('status');
