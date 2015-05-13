40e6e07100a10bb2a80ec256f31a60f47e7f286c
= <=>
2015-05-13 12:15:26 -0500

    finished budget reporting module. Budgets now report via web and excel

--- app/views/admin/projects/reportListing.blade.php
+++ app/views/admin/projects/reportListing.blade.php
@@ -4,7 +4,14 @@
 
 <h2 class="sub-header">Reports</h2>
     <div class="row">
-        <div class="col-xs-6"><a href="{{ URL::route("admin.projects.reports.budget", $semesterId) }}" class="btn btn-lg btn-block btn-default" style="text-align: left;"><i class="fa fa-usd text-success"></i> <b>Budget Report</b><br>Run a budget report for the entire semester based on current data</a></div>
+        <div class="col-xs-6">
+            <div class="btn-group btn-group-lg" role="group">
+                <a href="{{ URL::route("admin.projects.reports.budget", $semesterId) }}" class="btn btn-lg btn-default"> <b>Budget Report</b></a>
+                <a href="{{ URL::route("admin.projects.reports.budget",  array("sem_id" => $semesterId,"type"=>"excel")) }}" class="btn btn-lg btn-success" ><i class="fa fa-file-excel-o"></i> <b>Excel</b></a>
+
+
+            </div>
+        </div>
         <div class="col-xs-6"></div>
     </div>
 @stop
