091712ce046a734519ae772d7b86f53791f53bd1
Taylor Berg <me@tman0.com>
2019-04-12 15:06:57 -0500

    Update Idea Shop location in emails

--- resources/views/emails/printing/printed.blade.php
+++ resources/views/emails/printing/printed.blade.php
@@ -5,9 +5,18 @@
 @section('content')
 
     <h3>Hey there {{$person->FirstName}}!</h3>
-    <p>Our printer has printed your file and it is ready for pickup at the Idea Shop. The Idea Shop is located in the basement of the IIT Tower - Suite BC4-1. Take the elevators to the basement and turn right, can't miss it.</p>
-    <p>Please be sure to bring an ID to pickup your poster. We will have stations in the Idea Shop with the supplies necessary to mount your poster to foam core.</p>
-    <p>Please find a reference to the file below.</p>
+    <p>
+        Our printer has printed your file and it is ready for pickup at the Idea Shop.
+        Please come to the Idea Shop, room 116 in the Kaplan Institute during our normal hours.
+        You can check our hours on our calendar on the <a href="https://wiki.ideashop.iit.edu">Idea Shop Wiki</a>.
+    </p>
+    <p>
+        Please be sure to bring an ID to pickup your poster. We will have stations in the Idea Shop with the supplies
+        necessary to mount your poster to foam core.
+    </p>
+    <p>
+        Please find a reference to the file below.
+    </p>
     <table class="tableBlue" width="100%" cellpadding="3px" cellspacing="3px">
         <tr>
             <td>
--- resources/views/emails/printing/received.blade.php
+++ resources/views/emails/printing/received.blade.php
@@ -6,7 +6,7 @@
 
     <h3>Hey {{$person->FirstName}}!</h3>
     <p>We have received your poster submission and it is now in our Approval Queue. Once your poster is approved it will be sent to the the Print Shop. </p>
-    <p>You will receive an email to come pick up the print from the IdeaShop once your poster has been printed. We will supply you with foam core and spray glue to mount your poster. </p>
+    <p>You will receive an email to come pick up the print from the Idea Shop once your poster has been printed. We will supply you with foam core and spray glue to mount your poster. </p>
     <p>The details of your submission can be found below. Email us if you have any questions.</p>
     <table class="tableBlue" width="100%" cellpadding="3px" cellspacing="3px">
         <tr>
