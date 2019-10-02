fcd0390cb7fb8ef4f06de8b6641177015ef4209b
Taylor Berg <me@tman0.com>
2019-10-02 15:43:34 -0500

    Store entered comments in OrderNote

--- app/Http/Controllers/AdminItemController.php
+++ app/Http/Controllers/AdminItemController.php
@@ -1,8 +1,10 @@
 <?php
 
-class AdminItemController extends BaseController{
-    
-    public function editProcess($id){
+class AdminItemController extends BaseController
+{
+
+    public function editProcess($id)
+    {
         //Edit only the id passed in
         $item = Item::find($id);
         $item->Name = Input::get('Name');
@@ -10,7 +12,7 @@ class AdminItemController extends BaseController{
         $item->PartNumber = Input::get('PartNumber');
 
         //Let's see if the costs and quantities are changing and if we need to perform a gl entry
-        if(($item->Cost != Input::get('Cost')) || ($item->Quantity != Input::get('Quantity')) || ($item->Shipping != Input::get('Shipping'))){
+        if (($item->Cost != Input::get('Cost')) || ($item->Quantity != Input::get('Quantity')) || ($item->Shipping != Input::get('Shipping'))) {
             //Oh noes, we gotta figure out what to do next
             $originalTotalCost = ($item->Cost * $item->Quantity) + $item->Shipping;
             $newTotalCost = (Input::get('Cost') * Input::get('Quantity')) + Input::get('Shipping');
@@ -19,17 +21,17 @@ class AdminItemController extends BaseController{
             $order = $item->Order()->first();
             $project = $order->Project()->first();
             $account = $project->Account()->first();
-            if($originalTotalCost > $newTotalCost){
-                $account->Deposit('RECONCILE',$amount,$item->OrderID);
-            }elseif($originalTotalCost < $newTotalCost){
-                    if(!$account->Withdrawl('RECONCILE',$amount,$item->id)){
-                        return Redirect::to('/admin/orders/'.$item->OrderID)->with('error',array('Insufficient funds to cover changes'));
-                    }
+            if ($originalTotalCost > $newTotalCost) {
+                $account->Deposit('RECONCILE', $amount, $item->OrderID);
+            } elseif ($originalTotalCost < $newTotalCost) {
+                if (!$account->Withdrawl('RECONCILE', $amount, $item->id)) {
+                    return Redirect::to('/admin/orders/' . $item->OrderID)->with('error', array('Insufficient funds to cover changes'));
+                }
             }
             $orderNote = new OrderNote;
             $orderNote->OrderID = $item->OrderID;
             $orderNote->ItemID = $item->id;
-            $orderNote->Notes = "Item cost has changed from $".number_format($originalTotalCost,2)." to $".  number_format($newTotalCost,2);
+            $orderNote->Notes = "Item cost has changed from $" . number_format($originalTotalCost, 2) . " to $" . number_format($newTotalCost, 2);
             $orderNote->save();
         }
         $item->Cost = Input::get('Cost');
@@ -37,76 +39,79 @@ class AdminItemController extends BaseController{
         $item->shipping = Input::get('Shipping');
         $item->TotalCost = (Input::get('Cost') * Input::get('Quantity')) + Input::get('Shipping');
         $item->Justification = Input::get('Justification');
-        if($item->save()){
+        if ($item->save()) {
             //Save successful
             Order::recalculate($item->OrderID);
-            return Redirect::to('/admin/orders/'.$item->OrderID)->with('success',array('Successfully edited order item'));
-        }else{
+            return Redirect::to('/admin/orders/' . $item->OrderID)->with('success', array('Successfully edited order item'));
+        } else {
             //Save errors, redirect to the order page or a special item edit page
-            return Redirect::to('/admin/orders/'.$item->OrderID)->with('errors',array('Could not edit item'));
+            return Redirect::to('/admin/orders/' . $item->OrderID)->with('errors', array('Could not edit item'));
         }
     }
 
-    public function markItemReturning($id){
+    public function markItemReturning($id)
+    {
         $item = Item::find($id);
         $item->markReturning();
-        if($item->save()){
+        if ($item->save()) {
             //Save successful
-            return Redirect::to('/admin/orders/'.$item->OrderID)->with('success',array('Successfully marked item returning'));
-        }else{
+            return Redirect::to('/admin/orders/' . $item->OrderID)->with('success', array('Successfully marked item returning'));
+        } else {
             //Save errors, redirect to the order page or a special item edit page
-            return Redirect::to('/admin/orders/'.$item->OrderID)->with('errors',array('Could not edit item'));
+            return Redirect::to('/admin/orders/' . $item->OrderID)->with('errors', array('Could not edit item'));
         }
     }
-   
-    
-    public function markItemNotReturning($id){
+
+
+    public function markItemNotReturning($id)
+    {
         $item = Item::find($id);
         $item->Returning = false;
-        if($item->save()){
+        if ($item->save()) {
             //Save successful
-            return Redirect::to('/admin/orders/'.$item->OrderID)->with('success',array('Successfully marked item not returning'));
-        }else{
+            return Redirect::to('/admin/orders/' . $item->OrderID)->with('success', array('Successfully marked item not returning'));
+        } else {
             //Save errors, redirect to the order page or a special item edit page
-            return Redirect::to('/admin/orders/'.$item->OrderID)->with('errors',array('Could not edit item'));
+            return Redirect::to('/admin/orders/' . $item->OrderID)->with('errors', array('Could not edit item'));
         }
     }
-    
-    public function massStatusChangeProcess(){
+
+    public function massStatusChangeProcess()
+    {
         //This function will change the status for many items within an order
         //Let's see if we can grab the array of items
         $itemArray = json_decode(Input::get('massItemID'));
-        if(sizeof($itemArray) == 0){
-            return Redirect::to('/admin/orders/')->with('error',array('No items passed to mass status change'));
+        if (sizeof($itemArray) == 0) {
+            return Redirect::to('/admin/orders/')->with('error', array('No items passed to mass status change'));
         }
         $newStatus = Input::get('Status');
         $orderID = null;
         $itemCollection = array();
         $comment = Input::get('Comment');
-        foreach($itemArray as $itemID){
+        foreach ($itemArray as $itemID) {
             $item = Item::find($itemID);
-            array_push($itemCollection,$item);
-            }
+            array_push($itemCollection, $item);
+        }
         //Check that all items have the same order number & other validations
         $orderID = $itemCollection[0]->OrderID;
-        foreach($itemCollection as $item){
-            if($item->OrderID != $orderID){
-                return Redirect::to('/admin/orders/')->with('error',array('Items are from different orders'));
+        foreach ($itemCollection as $item) {
+            if ($item->OrderID != $orderID) {
+                return Redirect::to('/admin/orders/')->with('error', array('Items are from different orders'));
             }
         }
         //Now we need to take each item, update the status, save it, create an email listing and we are in business
-        foreach($itemCollection as $item){
-            $item->changeStatus($newStatus);
+        foreach ($itemCollection as $item) {
+            $item->changeStatus($newStatus, $comment);
         }
         //After we are done let's update the order
         Order::recalculate($orderID);
         $order = Order::find($orderID);
         $user = $order->User()->first();
         //Prepare the email, different emails based on the status we are updating to. 
-        switch($newStatus){
+        switch ($newStatus) {
             case 2: // approved for reimbursement
                 Mail::send('emails.orderSelfPurchase',
-                    array('person'=>$user, 'order'=>$order, 'items'=>$itemCollection, 'comment'=>$comment),
+                    array('person' => $user, 'order' => $order, 'items' => $itemCollection, 'comment' => $comment),
                     function ($message) use ($user) {
                         $message->to($user->Email, $user->FirstName . ' ' . $user->LastName);
                         $message->subject('IPRO order approved for reimbursement!');
@@ -114,17 +119,17 @@ class AdminItemController extends BaseController{
                 break;
             case 3: // ordered
                 Mail::send('emails.orderOrdered',
-                    array('person'=>$user,'order'=>$order,'items'=>$itemCollection, 'comment'=>$comment),
-                    function($message) use($user){
-                        $message->to($user->Email,$user->FirstName.' '.$user->LastName);
+                    array('person' => $user, 'order' => $order, 'items' => $itemCollection, 'comment' => $comment),
+                    function ($message) use ($user) {
+                        $message->to($user->Email, $user->FirstName . ' ' . $user->LastName);
                         $message->subject('IPRO order purchased!');
                     });
                 break;
             case 4: // received
                 Mail::send('emails.orderPickup',
-                    array('person'=>$user,'order'=>$order,'items'=>$itemCollection, 'comment'=>$comment),
-                    function($message) use($user){
-                        $message->to($user->Email,$user->FirstName.' '.$user->LastName)->subject('IPRO order ready for pickup!');
+                    array('person' => $user, 'order' => $order, 'items' => $itemCollection, 'comment' => $comment),
+                    function ($message) use ($user) {
+                        $message->to($user->Email, $user->FirstName . ' ' . $user->LastName)->subject('IPRO order ready for pickup!');
                     });
                 break;
             case 5: // picked up
@@ -132,7 +137,7 @@ class AdminItemController extends BaseController{
                 break;
             case 6: // denied
                 Mail::send('emails.orderCancelled',
-                    array('person'=>$user, 'order'=>$order, 'items'=>$itemCollection, 'comment'=>$comment),
+                    array('person' => $user, 'order' => $order, 'items' => $itemCollection, 'comment' => $comment),
                     function ($message) use ($user) {
                         $message->to($user->Email, $user->FirstName . ' ' . $user->LastName);
                         $message->subject('IPRO order cancelled');
@@ -140,7 +145,7 @@ class AdminItemController extends BaseController{
                 break;
             case 7: // check idea shop stock
                 Mail::send('emails.orderCheckStock',
-                    array('person'=>$user, 'order'=>$order, 'items'=>$itemCollection, 'comment'=>$comment),
+                    array('person' => $user, 'order' => $order, 'items' => $itemCollection, 'comment' => $comment),
                     function ($message) use ($user) {
                         $message->to($user->Email, $user->FirstName . ' ' . $user->LastName);
                         $message->subject('IPRO order update');
@@ -148,7 +153,7 @@ class AdminItemController extends BaseController{
                 break;
             case 8: // check buying restrictions
                 Mail::send('emails.orderCheckRestrictions',
-                    array('person'=>$user, 'order'=>$order, 'items'=>$itemCollection, 'comment'=>$comment),
+                    array('person' => $user, 'order' => $order, 'items' => $itemCollection, 'comment' => $comment),
                     function ($message) use ($user) {
                         $message->to($user->Email, $user->FirstName . ' ' . $user->LastName);
                         $message->subject('IPRO order update');
@@ -157,45 +162,48 @@ class AdminItemController extends BaseController{
 
 
         }
-        
-        return Redirect::to('/admin/orders/'.$orderID)->with('success',array('Successfully changed item statuses'));
+
+        return Redirect::to('/admin/orders/' . $orderID)->with('success', array('Successfully changed item statuses'));
     }
-    
-    public function massMarkReturningProcess(){
+
+    public function massMarkReturningProcess()
+    {
         //This function will change the status for many items within an order
         //Let's see if we can grab the array of items
         $itemArray = json_decode(Input::get('massItemID'));
-        if(sizeof($itemArray) == 0){
-            return Redirect::to('/admin/orders/')->with('error',array('No items passed to mass returning'));
+        if (sizeof($itemArray) == 0) {
+            return Redirect::to('/admin/orders/')->with('error', array('No items passed to mass returning'));
         }
         $orderID = null;
-        foreach($itemArray as $itemID){
+        foreach ($itemArray as $itemID) {
             $item = Item::find($itemID);
             $orderID = $item->OrderID;
             $item->markReturning();
             $item->save();
         }
-        return Redirect::to('/admin/orders/'.$orderID)->with('success',array('Successfully changed item statuses'));
+        return Redirect::to('/admin/orders/' . $orderID)->with('success', array('Successfully changed item statuses'));
     }
-    
+
     //Delete item code
-    public function deleteItem($id){
+    public function deleteItem($id)
+    {
         //Find the item, delete all notes relating to item
         $item = Item::find($id);
-        
+
         $orderID = $item->OrderID;
-        if($item->delete()){
+        if ($item->delete()) {
             Order::recalculate($orderID);
             $project = Order::find($orderID)->Project()->first();
             $account = $project->Account()->first();
             $account->Deposit('RECONCILE', (($item->Cost * $item->Quantity) + $item->Shipping), $orderID);
-            return Redirect::to('/admin/orders/'.$orderID)->with('success',array('Successfully deleted item'));
-        }else{
-            return Redirect::to('/admin/orders/'.$orderID)->with('error',array('Unable to delete item'));
+            return Redirect::to('/admin/orders/' . $orderID)->with('success', array('Successfully deleted item'));
+        } else {
+            return Redirect::to('/admin/orders/' . $orderID)->with('error', array('Unable to delete item'));
         }
     }
-    
-    public function printLabels(){
+
+    public function printLabels()
+    {
         //Someone has initalized a print labels 
         //The process for printint labels is to first get the items we are printing labels for,
         //Take those items and check for barcodes, if no barcode exists we generate one and then
@@ -204,17 +212,17 @@ class AdminItemController extends BaseController{
         //Format for input should be passed as a post named items and in array form containing the itemIDs;
         $items = json_decode(Input::get('items'));
         //Loop through the items and print labels
-        if(count($items) == 0){
+        if (count($items) == 0) {
             exit;
         }
         //Grab the item listing from the array
-        $itemsCollection = Item::whereIn('id',$items)->get();
+        $itemsCollection = Item::whereIn('id', $items)->get();
         //Loop through the item collection and generate barcodes for items that need them
 
-        foreach($itemsCollection as $item){
+        foreach ($itemsCollection as $item) {
             //Check that the item has a barcode
 
-            if($item->barcode == NULL){
+            if ($item->barcode == NULL) {
                 //Make le barcode, and if we make le barcode we should also change the item status to
                 $barcode = new Barcode;
                 $barcode->type = "ITEM";
@@ -226,7 +234,7 @@ class AdminItemController extends BaseController{
             $item->changeStatus(4);
             $item->save();
         }
-        View::share('items',$itemsCollection);
+        View::share('items', $itemsCollection);
         return View::make('admin.orders.print_label');
     }
 }
